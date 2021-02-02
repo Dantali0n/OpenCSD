33ccc31ba5a31f57e843c56dcc490929dde70e8c
Dantali0n <corne.lukken@hotmail.com>
2021-02-02 22:24:38 +0100

    Build libbpf and playground

---
+++ playground/libbpf/playground-libbpf.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <bpf/libbpf.h>
+
+int main(int argc, char* argv[]) {
+	std::cout << "Has AirGlow file: " << std::endl;
+	std::cout << "Has AirGlow output: " << std::endl;
+}
