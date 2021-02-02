11f8a8c1aabd6211d5f0b74f4dfbe5546ef9e392
Dantali0n <corne.lukken@hotmail.com>
2021-02-02 15:18:05 +0100

    Disable old playground & fix LaTeX documentation

--- playground/example/playground-example.cxx
+++ playground/example/playground-example.cxx
@@ -3,8 +3,8 @@
 #include "arguments.hpp"
 
 int main(int argc, char* argv[]) {
-	airglow::arguments::options opts{};
-	airglow::arguments::parse_args(argc, argv, &opts);
+	qemucsd::arguments::options opts{};
+	qemucsd::arguments::parse_args(argc, argv, &opts);
 	std::cout << "Has AirGlow file: " << opts.settings->c_str() << std::endl;
 	std::cout << "Has AirGlow output: " << opts.window_mode << std::endl;
 }
