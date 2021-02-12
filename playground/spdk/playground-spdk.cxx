d5796e89e401762c34745dd536c4fec012db92b1
Dantali0n <corne.lukken@hotmail.com>
2021-02-12 10:01:41 +0100

    Start of dpdk dependency

--- playground/spdk/playground-spdk.cxx
+++ playground/spdk/playground-spdk.cxx
@@ -7,5 +7,15 @@
 #include "spdk/env.h"
 
 int main(int argc, char* argv[]) {
+	int rc;
+	struct spdk_env_opts opts;
 
+	spdk_env_opts_init(&opts);
+
+	opts.name = "hello_world";
+	opts.shm_id = 0;
+	if (spdk_env_init(&opts) < 0) {
+		fprintf(stderr, "Unable to initialize SPDK env\n");
+		return 1;
+	}
 }
