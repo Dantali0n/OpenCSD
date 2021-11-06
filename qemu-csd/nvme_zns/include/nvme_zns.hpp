7c351136490d2adfce8868104d974e379dc36974
Dantali0n <info@dantalion.nl>
2021-11-06 08:04:04 +0100

    Abstract NVMe ZNS backends for decoupling

---
+++ qemu-csd/nvme_zns/include/nvme_zns.hpp
@@ -0,0 +1,67 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021 Dantali0n
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef QEMU_CSD_NVME_ZNS_HPP
+#define QEMU_CSD_NVME_ZNS_HPP
+
+#include "nvme_zns_backend.hpp"
+
+namespace qemucsd::nvme_zns {
+
+    /**
+     * Basic structure that should be made available to users of NvmeZns
+     * instances.
+     */
+    struct nvme_zns_info {
+        // Number of zones
+        uint64_t num_zones;
+        // Number of maximum open zones
+        uint64_t max_open;
+
+        // In bytes
+        uint64_t sector_size;
+        // In number of sectors
+        uint64_t zone_size;
+    };
+
+    template<class nvme_zns_backend>
+    class NvmeZns {
+        static_assert(
+            std::is_base_of<NvmeZnsBackend, nvme_zns_backend>::value,
+            "template argument nvme_zns_backend must inherit NvmeZnsBackend");
+    protected:
+        nvme_zns_backend* backend;
+    public:
+        NvmeZns(nvme_zns_backend* backend);
+
+        int read(uint64_t zone, uint64_t sector, size_t offset, void* buffer, size_t size);
+        int append(uint64_t zone, uint64_t& sector, size_t offset, void* buffer, size_t size);
+        int reset(uint64_t zone);
+    };
+
+    #include "nvme_zns.tpp"
+
+}
+
+#endif // QEMU_CSD_NVME_ZNS_HPP
