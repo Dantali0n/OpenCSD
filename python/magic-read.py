2d9b13a000fb0d788417cb180d69c9a7ed39d5cd
Dantali0n <info@dantalion.nl>
2022-01-14 22:05:59 +0100

    Improve python CSD example

--- python/magic-read.py
+++ python/magic-read.py
@@ -7,11 +7,17 @@ import pdb; pdb.set_trace()
 f = open("test/test", "rb")
 print(f.read())
 
+# Reset read back to beginning of file
+f.seek(0)
+
 # Get the inode number for the BPF kernel
 kern_ino = os.stat("test/kernel").st_ino
 
 # Enable the BPF kernel on the open file for read operations
 xattr.setxattr("test/test", "user.process.csd_read", f"{kern_ino}")
+
+# Verify the BPF kernel is set
+print(xattr.getxattr("test/test", "user.process.csd_read"))
 
 # Read the file again, this time the kernel will be executed
 print(f.read())
