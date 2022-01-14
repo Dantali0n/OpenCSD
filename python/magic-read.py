5900a4217faba13a7c5f0a7ef4dd11b57dc215af
Dantali0n <info@dantalion.nl>
2022-01-14 11:42:40 +0100

    Python example to configure kernels through inodes

--- python/magic-read.py
+++ python/magic-read.py
@@ -1,9 +1,20 @@
+import os
 import xattr
 
-def enable_csd_kernel():
-    xattr.setxattr("test/test", "user.process.csd_read", "yes")
-
 import pdb; pdb.set_trace()
+
+# Open the file and read it normally
 f = open("test/test", "rb")
 print(f.read())
+
+# Get the inode number for the BPF kernel
+kern_ino = os.stat("test/kernel").st_ino
+
+# Enable the BPF kernel on the open file for read operations
+xattr.setxattr("test/test", "user.process.csd_read", f"{kern_ino}")
+
+# Read the file again, this time the kernel will be executed
+print(f.read())
+
+# Close the file will release the kernel
 f.close()
