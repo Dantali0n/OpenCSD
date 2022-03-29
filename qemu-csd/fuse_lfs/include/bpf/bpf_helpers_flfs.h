807f9b57f9cb86e2a95d503ba712d41bfa59481a
Dantali0n <info@dantalion.nl>
2022-03-29 15:22:03 +0200

    A whole day for a simple write event kernel interface..

--- qemu-csd/fuse_lfs/include/bpf/bpf_helpers_flfs.h
+++ qemu-csd/fuse_lfs/include/bpf/bpf_helpers_flfs.h
@@ -31,8 +31,8 @@
  * FluffleFS I/O operations as exposed to BPF kernels
  */
 enum flfs_operations {
-    FLFS_READ  = 0,
-    FLFS_WRITE = 1,
+    FLFS_READ_STREAM  = 0,
+    FLFS_WRITE_EVENT = 1,
 };
 
 /**
@@ -71,21 +71,85 @@ struct __attribute__((packed)) flfs_call {
     struct dimensions dims;
     struct inode ino;
     // After this struct is initial_data_lba and the subsequent flattened data
-    // blocks
+    // blocks (for reads)
+    // Or flfs_write_call + initial_data_lba and the subsequent flattened data
+    // blocks (for writes)
+};
+
+/**
+ * Additional datastructure presented to kernels after flfs_call
+ */
+struct __attribute__((packed)) flfs_write_call {
+    /**
+     * pre and post pad ensure start and stop of actual write data can be found
+     * even when data inside the buffer is padded to be sector aligned. Pre and
+     * post pad will only be non zero if there is pre-existing data inside the
+     * section of the file to be written.
+     *
+     * This can prevent situations such as accidentally compressing small
+     * ( < 1 sector ) sections of data twice.
+     *
+     *           | Actual written request data   |
+     * | ............ |      |      |      | .............. |
+     * ^         ^                               ^          ^
+     * | pre_pad |                               | post_pad |
+     */
+    // TODO(Dantali0n): Support (partially) overwriting kernels, until then
+    //                  post pad will always be zero. This requires shifting
+    //                  data_block LBAs, an extensive feature.
+    uint64_t pre_pad;
+    uint64_t post_pad;
+    // TODO(Dantali0n): Kernels running on the device should be able to
+    //                  intrinsically know written sectors in zones.
+    //                  Requires changes to nvme_zns interface.
+    /**
+     * | RANDOM ZONE | LOG ZONE                |
+     *               ^          ^              ^
+     *               |          |              |
+     *           start_ptr    wp_ptr        end_ptr
+     */
+    uint64_t start_ptr;
+    uint64_t wp_ptr;
+    uint64_t end_ptr;
+};
+
+/**
+ * Status indicator of write kernel
+ */
+enum write_return_status {
+    WRITE_SUCCESS = 0,
+    WRITE_FAIL = 1,
+    // Must return full even if last append is successful!
+    WRITE_DEVICE_FULL_SUCCESS  = 2,
+    WRITE_DEVICE_FULL_FAIL  = 3,
+};
+
+/**
+ * This data struct should be returned by write kernels
+ */
+struct __attribute__((packed)) flfs_write_return {
+    enum write_return_status status;
+    // Size of the write request after kernel processing, can be more or less.
+    uint64_t size;
+    // After this are the flattened LBas of kernel written data
 };
 
 /**
  * Find the flattened data blocks FluffleFS provides in filesystem specific
- * kernel data
+ * kernel data.
  * @param call_info parameter with filesystem specific data as can be retrieved
  *        from bpf_get_call_info defined in bpf_helpers_prog.h
  */
-static void find_data_lba(uint64_t **call_info) {
+static void find_data_lba(uint64_t **call_info, bool write = false) {
     *call_info = (uint64_t*)((uint8_t*)*call_info + sizeof(struct flfs_call));
+
+    if(write)
+        *call_info = (uint64_t*)((uint8_t*)*call_info +
+            sizeof(struct flfs_write_call));
 }
 
 /**
- * Increment the cur_data_lba pointer and indicate if next data block exists
+ * Increment the cur_data_lba pointer and indicate if next data block exists.
  * @param cur_data_lba the current location of valid data for the given inode
  */
 static void next_data_lba(uint64_t **cur_data_lba) {
