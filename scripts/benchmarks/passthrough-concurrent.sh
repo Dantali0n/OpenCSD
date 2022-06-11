c9923586d07b001d53bc7d3689811f5e560130ea
Dantali0n <info@dantalion.nl>
2022-06-11 14:07:44 +0200

    Update the passthrough and concurrent benchmark scripts

--- scripts/benchmarks/passthrough-concurrent.sh
+++ scripts/benchmarks/passthrough-concurrent.sh
@@ -4,24 +4,44 @@
 # performance due to N incoming parallel writes.
 #
 
-for size in 64k 256k 1024k 4096k 16384k 65536k 262144k 1048576k
+pids=()
+
+cat >> fio.job<< EOF
+[global]
+filename=test/test
+
+[randwrite]
+rw=randwrite
+bs=524288
+EOF
+
+for size in 1024k 4096k 16384k 65536k 262144k 1048576k
 do
       echo "passthrough concurrent --size=${size} ..."
       echo "bandwidth" > passthrough-read-concurrent-${size}.csv
-      for _ in {1..1}
+      for _ in {1..30}
       do
-          ./fuse-entry -- -d -o max_read=2147483647 test > /dev/null 2>&1 &
-          pid=$!
-          sleep 3
-          cp bin/bpf_* test/
-          head -c ${size} </dev/urandom > test/test
-          for _ in {1..4}
+          ./fuse-entry-spdk -- -d -o max_read=2147483647 test > /dev/null 2>&1 &
+          fuse_pid=$!
+          sleep 5
+          cp bin/bpf_* test/ || exit 1
+          head -c ${size} </dev/urandom > test/test || exit 1
+          for _ in {1..1}
           do
-              head -c ${size} </dev/urandom > test/test &
+              # head -c ${size} </dev/urandom > test/test &
+              fio --name=global --rw=randwrite --size=${size} --bs=524288 --output-format=json+ fio.job > /dev/null 2>&1 &
+              pids+=($!)
           done
           results=$(python ../../python/csd-read-passthrough.py)
           export size_normal=${size:0:-1}
           echo "$results" | grep "Wall time:" | awk '{print $3}' | awk -F"E" 'BEGIN{OFMT="%10.10f"} {print (ENVIRON["size_normal"] * 1024) / ($1 * (10 ^ $2))}' >> passthrough-read-concurrent-${size}.csv
-          kill $pid
+          for pid in "${pids[@]}"; do
+            kill $pid
+            tail --pid=$pid -f /dev/null
+          done
+          pids=()
+          kill $fuse_pid || exit 1
+          tail --pid=$fuse_pid -f /dev/null
+          sleep 30
       done
 done
---
+++ scripts/qemu-start-real-zns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ls -l /sys/block/nvme5n1/device/device
+echo "0000:61:00.0" | sudo tee /sys/bus/pci/drivers/nvme/unbind
+sudo modprobe vfio-pci
+# lspci -n -s 0000:61:00.0
+#$ 61:00.0 0108: 1b96:2600
+echo 1b96 2600 | sudo tee /sys/bus/pci/drivers/vfio-pci/new_id
+
+nohup qemu-system-x86_64 -nographic -name qemucsd -m 32G --enable-kvm -cpu host -smp 8 \
+-hda ./arch-qemucsd.qcow2 \
+-net user,hostfwd=tcp:127.0.0.1:7777-:22,hostfwd=tcp:127.0.0.1:2222-:2000 -net nic \
+-device vfio-pci,host=0000:61:00.0 & echo $! > qemu.pid
