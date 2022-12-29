abbb33c5ab2f81616f8a17dcbbbf5a190b25b2b8
Dantali0n <info@dantalion.nl>
2022-12-29 10:57:53 +0100

    Fix codecov reporting for public detailed online reports

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -39,6 +39,9 @@
 # 2021-02-25, Corne Lukken
 # - Changed cobertura to use lcov-to-cobertura instead of gcovr directly.
 #
+# 2021-12-29, Corne Lukken
+# - Removed dead code
+#
 # USAGE:
 
 # 0. (Mac only) If you use Xcode 5.1 make sure to patch geninfo as described here:
@@ -89,82 +92,9 @@ ELSEIF(NOT CMAKE_COMPILER_IS_GNUCXX)
     MESSAGE(FATAL_ERROR "Compiler is not GNU gcc! Aborting...")
 ENDIF() # CHECK VALID COMPILER
 
-SET(CMAKE_CXX_FLAGS_COVERAGE
-        "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
-        CACHE STRING "Flags used by the C++ compiler during coverage builds."
-        FORCE )
-SET(CMAKE_C_FLAGS_COVERAGE
-        "-g -O0 --coverage -fprofile-arcs -ftest-coverage"
-        CACHE STRING "Flags used by the C compiler during coverage builds."
-        FORCE )
-SET(CMAKE_EXE_LINKER_FLAGS_COVERAGE
-        ""
-        CACHE STRING "Flags used for linking binaries during coverage builds."
-        FORCE )
-SET(CMAKE_SHARED_LINKER_FLAGS_COVERAGE
-        ""
-        CACHE STRING "Flags used by the shared libraries linker during coverage builds."
-        FORCE )
-MARK_AS_ADVANCED(
-        CMAKE_CXX_FLAGS_COVERAGE
-        CMAKE_C_FLAGS_COVERAGE
-        CMAKE_EXE_LINKER_FLAGS_COVERAGE
-        CMAKE_SHARED_LINKER_FLAGS_COVERAGE )
-
 IF ( NOT (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "Coverage"))
     MESSAGE( WARNING "Code coverage results with an optimized (non-Debug) build may be misleading" )
 ENDIF() # NOT CMAKE_BUILD_TYPE STREQUAL "Debug"
-
-
-# Param _targetname     The name of new the custom make target
-# Param _testrunner     The name of the target which runs the tests.
-#						MUST return ZERO always, even on errors.
-#						If not, no coverage report will be created!
-# Param _outputname     lcov output is generated as _outputname.info
-#                       HTML report is generated in _outputname/index.html
-# Optional fourth parameter is passed as arguments to _testrunner
-#   Pass them in list form, e.g.: "-j;2" for -j 2
-FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
-
-    IF(NOT LCOV_PATH)
-        MESSAGE(FATAL_ERROR "lcov not found! Aborting...")
-    ENDIF() # NOT LCOV_PATH
-
-    IF(NOT GENHTML_PATH)
-        MESSAGE(FATAL_ERROR "genhtml not found! Aborting...")
-    ENDIF() # NOT GENHTML_PATH
-
-    SET(coverage_info "${CMAKE_BINARY_DIR}/${_outputname}.info")
-    SET(coverage_cleaned "${coverage_info}.cleaned")
-
-    SEPARATE_ARGUMENTS(test_command UNIX_COMMAND "${_testrunner}")
-
-    # Setup target
-    ADD_CUSTOM_TARGET(${_targetname}
-
-        # Cleanup lcov
-        ${LCOV_PATH} --directory . --zerocounters
-
-        # Run tests
-        COMMAND ${test_command} ${ARGV3}
-
-        # Capturing lcov counters and generating report
-        COMMAND ${LCOV_PATH} --directory . --capture --output-file ${coverage_info}
-        COMMAND ${LCOV_PATH} --remove ${coverage_info} '${CMAKE_BINARY_DIR}/qemu-csd/include/*' '${CMAKE_SOURCE_DIR}/tests/*' '/usr/*' --output-file ${coverage_cleaned}
-        COMMAND ${GENHTML_PATH} -o ${_outputname} ${coverage_cleaned}
-        COMMAND ${CMAKE_COMMAND} -E remove ${coverage_info} ${coverage_cleaned}
-
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        COMMENT "Resetting code coverage counters to zero.\n Processing code coverage counters and generating report."
-    )
-
-    # Show info where to find the report
-    ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-        COMMAND ;
-        COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
-    )
-
-ENDFUNCTION() # SETUP_TARGET_FOR_COVERAGE
 
 # Param _targetname     The name of new the custom make target
 # Param _testrunner     The name of the target which runs the tests
@@ -206,7 +136,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE_LCOV _targetname _testrunner _outputname)
         COMMAND ${LCOV_PATH} --remove ${coverage_total} '${CMAKE_BINARY_DIR}/qemu-csd/include/*' '${CMAKE_SOURCE_DIR}/dependencies/*' '${CMAKE_SOURCE_DIR}/tests/*' '/usr/*' --output-file ${coverage_cleaned}
         COMMAND ${GENHTML_PATH} -o ${_outputname} ${coverage_cleaned}
         COMMAND ${CMAKE_SOURCE_DIR}/python/lcov-to-cobertura/lcov_cobertura/lcov_cobertura.py ${coverage_cleaned}
-        COMMAND ${CMAKE_COMMAND} -E remove ${coverage_info} ${coverage_cleaned}
+        COMMAND ${CMAKE_COMMAND} -E remove ${coverage_info} ${coverage_base}
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT "Running gcovr to produce Cobertura code coverage report."
     )
